e2da15e396957a44a9d258dea1ab5d31731fddb7
hojin <ahnhojin1026@naver.com>
2024-06-02 22:02:57 +0900

    1181 done

--- sliver5/1181.c
+++ sliver5/1181.c
@@ -2,58 +2,120 @@
 #include <stdlib.h>
 #include <string.h>
 
+int partition (char** arr, int* arr_n, int p, int r){
+    int low, high;
+    int pivot = arr_n[p];
+
+    low = p+1;
+    high = r;
+
+    while(low <= high){
+        while(low <= r && arr_n[low] <= pivot) low++; // pivot 보다 작은 값이 나올때마다 이동
+        while(high >= p + 1 && arr_n[high] > pivot) high--; // pivot 보다 큰 값이 나올때마다 이동
+ 
+        if (low <= high){ // low와 high 가 중단된 지점이 서로 위치가 역전된 지점이 아니라면
+            char* temp = arr[low];    // low 와 high 의 값 변경
+            arr[low] = arr[high];
+            arr[high] = temp;
+            int temp_n = arr_n[low];    // low 와 high 의 값 변경
+            arr_n[low] = arr_n[high];
+            arr_n[high] = temp_n;
+        }
+    }
+    // 피벗과 high 위치 교환
+    int temp_n = arr_n[p];
+    arr_n[p] = arr_n[high];
+    arr_n[high] = temp_n;
+    char* temp = arr[p];
+    arr[p] = arr[high];
+    arr[high] = temp;
+
+    return high; // 피벗 위치 반환
+}
+int partition1 (char** arr, int* arr_n, int p, int r){
+    int low, high;
+    char* pivot = arr[p];
+
+    low = p+1;
+    high = r;
+
+    while(low <= high){
+        while(low <= r && strcmp(arr[low],pivot)<=0) low++; // pivot 보다 작은 값이 나올때마다 이동
+        while(high >= p + 1 && strcmp(arr[high],pivot)>0) high--; // pivot 보다 큰 값이 나올때마다 이동
+ 
+        if (low <= high){ // low와 high 가 중단된 지점이 서로 위치가 역전된 지점이 아니라면
+            char* temp = arr[low];    // low 와 high 의 값 변경
+            arr[low] = arr[high];
+            arr[high] = temp;
+            int temp_n = arr_n[low];    // low 와 high 의 값 변경
+            arr_n[low] = arr_n[high];
+            arr_n[high] = temp_n;
+        }
+    }
+    // 피벗과 high 위치 교환
+    int temp_n = arr_n[p];
+    arr_n[p] = arr_n[high];
+    arr_n[high] = temp_n;
+    char* temp = arr[p];
+    arr[p] = arr[high];
+    arr[high] = temp;
+
+    return high; // 피벗 위치 반환
+}
+
+void quick_sort(char** arr, int* arr_n, int left, int right, int mode){
+    if(left < right){
+        // printf("start quick (%d %d)\n",left, right);
+        int pivot;
+        if(mode == 0){
+            pivot = partition(arr,arr_n,left,right);
+        }
+        else{
+            pivot = partition1(arr,arr_n,left,right);
+        }
+
+        quick_sort(arr,arr_n,left,pivot-1,mode);
+        quick_sort(arr,arr_n,pivot+1,right,mode);
+    }
+}
+
 int main () {
     int MAX_STRING_LEN = 50;
     int n;
     scanf("%d",&n);
     char** arr = (char **) malloc(sizeof(char*)*n);
-    char*** size_sort = (char ***) malloc(sizeof(char*));
-    int size_len[50] = {0};
-    for(int i=0;i<50;i++){
-        size_sort[i] = (char **) malloc(sizeof(char*));
-    }
+    int* arr_n = (int *) malloc(sizeof(int)*n);
     //first get the inputs by length
     for (int i = 0; i<n;i++){
         arr[i] = (char*) malloc(MAX_STRING_LEN * sizeof(char));
         scanf("%s", arr[i]);
-        int len = strlen(arr[i]);
-        size_sort[len] = (char**)realloc(size_sort[len], sizeof(char*)*(size_len[len]+1));
-        size_sort[len][size_len[len]] = arr[i];
-        size_len[len]++;
+        arr_n[i] = strlen(arr[i]);
     }
-    for(int i=0;i<50;i++){
-        //sort by each size
-        for(int j=0;j<size_len[i];j++){
-            char* min = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
-            int min_index;
-            for(int k=j;k<size_len[i];k++){
-                if(strcmp(size_sort[i][k],min)<0){
-                    min = size_sort[i][k];
-                    min_index = k;
-                }
-            }
-            char *tmp = size_sort[i][j];
-            size_sort[i][j] = min;
-            size_sort[i][min_index] = tmp;
+    //first sort by strlen
+    quick_sort(arr,arr_n,0,n-1,0);
+    int start = 0;
+    int end = 0;
+    while(start < n){
+        end = start;
+        while(arr_n[start] == arr_n[end]){
+            end++;
         }
+        quick_sort(arr,arr_n,start,end-1,1);
+        start = end;
     }
-    for (int i = 0; i < 50; i++) {
-        if (size_len[i] > 0) {
-            printf("%s\n", size_sort[i][0]);
-            for (int j = 1; j < size_len[i]; j++) {
-                if (strcmp(size_sort[i][j], size_sort[i][j - 1]) != 0) {
-                    printf("%s\n", size_sort[i][j]);
-                }
+    for(int i=0;i<n;i++){
+        if(n != 1 && i != n-1){
+            if(strcmp(arr[i],arr[i+1])!=0){
+                printf("%s\n",arr[i]);
             }
         }
-    }
-    for (int i = 0; i<n;i++){
+        else{
+            printf("%s\n",arr[i]);
+        }
         free(arr[i]);
     }
     free(arr);
-    for(int i=0;i<50;i++){
-        free(size_sort[i]);
-    }
-    free(size_sort);
+    free(arr_n);
+
     return 0;
 }
